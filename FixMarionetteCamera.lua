9bba999d3036f758fad3d370f1f1f116921a9d71
WetU <50101064+WetU@users.noreply.github.com>
2023-05-25 23:15:52 +0900

    Add files via upload

--- FixMarionetteCamera.lua
+++ FixMarionetteCamera.lua
@@ -1,29 +1,12 @@
 -- Initialize
-local pairs = pairs;
-
-local json = json;
-
 local sdk = sdk;
 local sdk_find_type_definition = sdk.find_type_definition;
 local sdk_get_managed_singleton = sdk.get_managed_singleton;
 local sdk_to_int64 = sdk.to_int64;
 
-local re = re;
-
-local imgui = imgui;
-local imgui_tree_node = imgui.tree_node;
-local imgui_checkbox = imgui.checkbox;
-local imgui_tree_pop = imgui.tree_pop;
-
-local settings = json.load_file("Fix_Marionette_Camera.json") or {enable = true};
-if settings.enable == nil then
-	settings.enable = true;
-end
 -- Cache
 local ResetState_None = sdk_find_type_definition("snow.camera.PlayerCamera.ResetState"):get_field("None"):get_data(nil);
-
 local get_MarionetteCameraType_method = sdk_find_type_definition("snow.CameraManager"):get_method("get_MarionetteCameraType"); -- retval
-
 local MarionetteCameraType_type_def = get_MarionetteCameraType_method:get_return_type();
 local NotResetTypes = {
 	[MarionetteCameraType_type_def:get_field("GetOff"):get_data(nil)] = true,
@@ -43,21 +26,4 @@ sdk.hook(sdk_find_type_definition("snow.camera.TargetCamera_Marionette"):get_met
 		end
 	end
 	return retval;
-end);
----- re Callbacks ----
-local function save_config()
-	json.dump_file("Fix_Marionette_Camera.json", settings);
-end
-
-re.on_config_save(save_config);
-
-re.on_draw_ui(function()
-	local changed = false;
-	if imgui_tree_node("Fix Marionette Camera") then
-		changed, settings.enable = imgui_checkbox("Enabled", settings.enable);
-		if changed then
-			save_config();
-		end
-		imgui_tree_pop();
-	end
 end);
