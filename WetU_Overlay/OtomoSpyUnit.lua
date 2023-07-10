caa66b7924147973f64f5ca209a87e6a537c1ea6
WetU <50101064+WetU@users.noreply.github.com>
2023-07-10 19:35:12 +0900

    Add files via upload

--- WetU_Overlay/OtomoSpyUnit.lua
+++ WetU_Overlay/OtomoSpyUnit.lua
@@ -15,8 +15,12 @@ local updateRewardList_method = GuiOtomoSpyUnitMainControll_type_def:get_method(
 local RewardListCursor_field = GuiOtomoSpyUnitMainControll_type_def:get_field("RewardListCursor");
 
 local RewardListCursor_type_def = RewardListCursor_field:get_type();
+local get__PageCursor_method = RewardListCursor_type_def:get_method("get__PageCursor");
 local setIndex_method = RewardListCursor_type_def:get_method("setIndex(via.vec2)");
-local setPageNo_method = RewardListCursor_type_def:get_method("setPageNo(System.Int32)");
+
+local PageCursor_type_def = get__PageCursor_method:get_return_type();
+local set_pageNo_method = PageCursor_type_def:get_method("set_pageNo(System.Int32)");
+local getPageMax_method = PageCursor_type_def:get_method("getPageMax");
 
 local GuiOtomoSpyUnitReturn_type_def = Constants.SDK.find_type_definition("snow.gui.fsm.otomospy.GuiOtomoSpyUnitReturn");
 --
@@ -84,10 +88,13 @@ local function handleReward(args)
     if GuiOtomoSpyUnitMainControll ~= nil then
         local RewardListCursor = RewardListCursor_field:get_data(GuiOtomoSpyUnitMainControll);
         if RewardListCursor ~= nil then
-            setPageNo_method:call(RewardListCursor, 5);
-            setIndex_method:call(RewardListCursor, ReceiveAllButton_Index);
-            updateRewardList_method:call(GuiOtomoSpyUnitMainControll);
-            isReceiveReady = true;
+            local PageCursor = get__PageCursor_method:call(RewardListCursor);
+            if PageCursor ~= nil then
+                set_pageNo_method:call(PageCursor, getPageMax_method:call(PageCursor));
+                setIndex_method:call(RewardListCursor, ReceiveAllButton_Index);
+                updateRewardList_method:call(GuiOtomoSpyUnitMainControll);
+                isReceiveReady = true;
+            end
         end
     end
 end
@@ -99,13 +106,9 @@ local function PreHook_getDecideButtonRep()
 end
 local function PostHook_getDecideButtonRep(retval)
     if isReceiveReady == true then
+        isReceiveReady = false;
         return Constants.TRUE_POINTER;
     end
-    return retval;
-end
-
-local function PostHook_addAllGameItemtoBox(retval)
-    isReceiveReady = false;
     return retval;
 end
 
@@ -128,7 +131,6 @@ function this.init()
     Constants.SDK.hook(GuiOtomoSpyUnitReturn_type_def:get_method("endOtomoSpyUnitReturn"), nil, PostHook_endOtomoSpyUnitReturn);
     Constants.SDK.hook(GuiOtomoSpyUnitMainControll_type_def:get_method("updateRewardListCursor"), handleReward);
     Constants.SDK.hook(Constants.type_definitions.StmGuiInput_type_def:get_method("getDecideButtonRep(snow.StmInputConfig.KeyConfigType, System.Boolean)"), PreHook_getDecideButtonRep, PostHook_getDecideButtonRep);
-    Constants.SDK.hook(GuiOtomoSpyUnitMainControll_type_def:get_method("addAllGameItemtoBox(System.Boolean)"), nil, PostHook_addAllGameItemtoBox);
     Constants.SDK.hook(Constants.type_definitions.QuestManager_type_def:get_method("onChangedGameStatus(snow.SnowGameManager.StatusType)"), onChangedGameStatus);
 end
 --
