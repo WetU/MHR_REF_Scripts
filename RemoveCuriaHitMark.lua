1ad4560bbc06062303b2e9277efd5c4b406718aa
WetU <50101064+WetU@users.noreply.github.com>
2023-07-24 18:03:02 +0900

    Delete RemoveCuriaHitMark.lua

--- RemoveCuriaHitMark.lua
+++
@@ -1,6 +0,0 @@
-local Constants = require("Constants.Constants");
-if Constants == nil then
-    return;
-end
---
-Constants.SDK.hook(Constants.SDK.find_type_definition("snow.enemy.EnemyCharacterBase"):get_method("requestMysteryCoreHitMark(snow.enemy.EnemyDef.PartsGroup, snow.hit.EnemyCalcDamageInfo.AfterCalcInfo_DamageSide)"), Constants.SKIP_ORIGINAL);
